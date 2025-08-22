e2cdb3a79330811faf293e9ba635e962ac456bb8
Connor Brown <cbrown0794@floridapoly.edu>
2025-08-22 10:37:04 -0400

    First Push
    First Push

---
+++ 01_intro_to_coding/first_program.c
@@ -0,0 +1,24 @@
+/*
+
+Multi-Line comment
+8/20/2025
+Connor Brown
+First program in C
+
+*/
+
+// One Line Comment
+
+// Pre-processor directive
+// stdio.h - standard input output
+// .h - header file
+#include <stdio.h>
+ 
+// Create the main function
+
+int main(void) {
+    printf("Hello\nWorld\n");
+    printf("I\nam\nready\nto\ncode!\n");
+
+    return 0;
+}
