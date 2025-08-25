6597d408c2cb93350decb32ea7b151e187a60e6c
Connor Brown <cbrown0794@floridapoly.edu>
2025-08-25 14:15:49 +0000

    8/25/2025

---
+++ 02_student_info/student_info.c
@@ -0,0 +1,23 @@
+/*
+
+Multi-Line comment
+8/22/2025
+Connor Brown
+Student Info
+
+*/
+
+// One Line Comment
+
+#include <stdio.h>
+ 
+
+int main (void){
+    int courseID = 2271;
+    int studentID = 20794;
+    printf("Name:\t\t\t\tConnor Brown\n");
+    printf("Course ID:\t\t\t%d\n",courseID);
+    printf("Student ID:\t\t\tU00000%d\n",studentID);
+    printf("Student ID - Course ID is:\t%d",studentID - courseID);
+
+}
